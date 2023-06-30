9f41260fb5d7ae3e3b9f17c76eb53067b334d5e9
maaaashi <doyaseizin.7@gmail.com>
2023-06-30 23:54:16 +0900

    markdownの導入

--- src/pages/about-me/index.tsx
+++ src/pages/about-me/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { aboutMeData } from '@/libs/about-me'
+import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
 
 const AboutMe = () => {
   return (
-    <div>
-      {aboutMeData()}
+    <div className='markdown'>
+      <ReactMarkdown remarkPlugins={[gfm]}>{aboutMeData()}</ReactMarkdown>
     </div>
   )
 }
