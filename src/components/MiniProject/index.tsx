06b00021bfa1cf0bc300194836a650e4d5160b18
maaaashi <doyaseizin.7@gmail.com>
2023-06-30 13:01:46 +0900

    mini project レイアウト調整

--- src/components/MiniProject/index.tsx
+++ src/components/MiniProject/index.tsx
@@ -7,11 +7,18 @@ import Image from 'next/image'
 interface Props {
   title: string
   href: string
+  description: string
   qiita?: string
   github?: string
 }
 
-const MiniProject: FC<Props> = ({ title, href, qiita, github }) => {
+const MiniProject: FC<Props> = ({
+  title,
+  href,
+  description,
+  qiita,
+  github,
+}) => {
   return (
     <div className='flex items-center gap-5 p-5'>
       <div className='w-96'>
@@ -19,20 +26,20 @@ const MiniProject: FC<Props> = ({ title, href, qiita, github }) => {
       </div>
       <div className='text-center'>
         <h3>{title}</h3>
-        <p>afe</p>
-        <p>afefefe</p>
-        <p>afefe</p>
+        <p>{description}</p>
 
-        <a href={github}>
-          <AiFillGithub size={40} />
-        </a>
-        <a href={qiita} className='h-fit w-fit'>
-          <SiQiita
-            size={40}
-            color='white'
-            style={{ backgroundColor: 'rgba(85, 198, 0)' }}
-          />
-        </a>
+        <div className='flex justify-evenly rounded-lg bg-base-300 p-5'>
+          <a target='_blank' href={github}>
+            <AiFillGithub size={40} />
+          </a>
+          <a target='_blank' href={qiita} className='h-fit w-fit'>
+            <SiQiita
+              size={40}
+              color='white'
+              style={{ backgroundColor: 'rgba(85, 198, 0)' }}
+            />
+          </a>
+        </div>
       </div>
     </div>
   )
--- src/pages/mini-project/index.tsx
+++ src/pages/mini-project/index.tsx
@@ -4,11 +4,12 @@ import MiniProject from '@/components/MiniProject'
 const Project = () => {
   return (
     <div>
-      <h2>Mini Project</h2>
+      <h2 className='text-2xl font-bold'>Mini Project</h2>
       <p>学習で個人的に作成したプロジェクトです。</p>
 
       <MiniProject
         title='感想文生成ジェネレーター'
+        description='設定から、感想文を自動生成するツール'
         href='https://opinion-ai.vercel.app/'
         github='https://github.com/maaaashi/opinion-ai'
         qiita='https://qiita.com/maaaashi/items/cbc06263c51ab64e5dbe'
