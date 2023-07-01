99b314f02a552f17936f81fe30fd9d56b79ba6e1
maaaashi <doyaseizin.7@gmail.com>
2023-07-01 21:40:08 +0900

    add type

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -11,7 +11,7 @@ function hslToRgb(h: number, s: number, l: number) {
   if (s === 0) {
     r = g = b = l // achromatic
   } else {
-    const hue2rgb = (p, q, t) => {
+    const hue2rgb = (p: number, q: number, t: number) => {
       if (t < 0) t += 1
       if (t > 1) t -= 1
       if (t < 1 / 6) return p + (q - p) * 6 * t
@@ -25,7 +25,7 @@ function hslToRgb(h: number, s: number, l: number) {
     g = hue2rgb(p, q, h)
     b = hue2rgb(p, q, h - 1 / 3)
   }
-  const toHex = (x) => {
+  const toHex = (x: number) => {
     const hex = Math.round(x * 255).toString(16)
     return hex.length === 1 ? '0' + hex : hex
   }
