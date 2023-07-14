2caa758ac634efea7198bd4b31506b83a3c5972e
maaaashi <doyaseizin.7@gmail.com>
2023-07-15 00:43:14 +0900

    meta 追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='ja'>
-      <Head />
+      <Head>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, shrink-to-fit=no'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
