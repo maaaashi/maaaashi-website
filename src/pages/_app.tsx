9714d443c3bb7b7ffb238158dc8fdfcd80963046
maaaashi <doyaseizin.7@gmail.com>
2023-07-17 17:32:41 +0900

    header, favicon追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import Header from '@/components/Header'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className='flex h-screen flex-col overflow-y-auto'>
+      <Head>
+        <title>{"Maaaashi's Website"}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
       <div className='sticky top-0 z-50'>
         <Header />
       </div>
