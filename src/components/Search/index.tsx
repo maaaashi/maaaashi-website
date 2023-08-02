69c07c2179eb407348c867097784be4e4a4a0143
maaaashi <doyaseizin.7@gmail.com>
2023-08-03 08:07:52 +0900

    検索欄デザイン調整

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -40,9 +40,9 @@ const Search: FC<Props> = ({ articles }) => {
 
   return (
     <div>
-      <h3 className='text-lg font-bold'>Search</h3>
-      <div className='form-control m-4 w-full max-w-xs'>
-        <div className='flex gap-3'>
+      <div className='flex items-center gap-4'>
+        <h3 className='text-lg font-bold'>Search</h3>
+        <div className='flex items-center gap-2'>
           <input
             type='checkbox'
             id='mode'
@@ -57,19 +57,18 @@ const Search: FC<Props> = ({ articles }) => {
             {titleMode ? 'タイトル検索' : '本文検索'}
           </label>
         </div>
-        <label className='label'>
-          <span className='label-text'>検索したい内容</span>
-        </label>
-        <form onSubmit={submitHandler} className='flex'>
+      </div>
+      <div className='form-control w-full p-4'>
+        <form onSubmit={submitHandler} className='flex flex-1'>
           <input
             type='text'
-            placeholder='Type here'
+            placeholder='検索したい内容を入力'
             value={userInput}
             required
             onChange={(e) => {
               setUserInput(e.target.value)
             }}
-            className='input-bordered input w-full max-w-xs'
+            className='input-bordered input w-full'
           />
           <button type='submit' className='btn-primary btn'>
             検索
