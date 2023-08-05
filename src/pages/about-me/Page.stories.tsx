2518e48d2ef6ddc184fba2f3481f4f9bffc1c21a
maaaashi <doyaseizin.7@gmail.com>
2023-08-05 10:28:33 +0900

    pageカタログ追加

---
+++ src/pages/about-me/Page.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Page from './index'
+
+const meta = {
+  component: Page,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Page>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const View: Story = {}
---
+++ src/pages/qiita/Page.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Page from './index'
+import { mockData } from '@/libs/mockdata'
+
+const meta = {
+  component: Page,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Page>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const responseMockData = mockData.map((article) => {
+  return {
+    rendered_body: article.rendered_body,
+    body: article.body,
+    comments_count: article.comments_count,
+    created_at: article.created_at,
+    likes_count: article.likes_count,
+    reactions_count: article.reactions_count,
+    stocks_count: article.stocks_count,
+    tags: article.tags,
+    title: article.title,
+    url: article.url,
+    page_views_count: article.page_views_count,
+  }
+})
+
+export const View: Story = {
+  args: {
+    articles: responseMockData,
+  },
+}
